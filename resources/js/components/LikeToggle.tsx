3bc4023d1b128aae165737420edddf0f661e000f
jkcachero <johnkarlo.315@gmail.com>
2025-12-11 20:06:57 +0800

    Last commit before toggle like

--- resources/js/components/LikeToggle.tsx
+++ resources/js/components/LikeToggle.tsx
@@ -1,13 +1,18 @@
 import { Dispatch, SetStateAction, useState } from "react";
 import { Heart, LoaderCircle } from "lucide-react";
-import { Puppy } from "../types";
+import { Puppy, SharedData } from "../types";
 import { toggleLikedStatus } from "../queries";
+import { usePage } from "@inertiajs/react";
+import clsx from "clsx";
 
 export function LikeToggle({ puppy, setPuppies }: { puppy: Puppy, setPuppies: Dispatch<SetStateAction<Puppy[]>> }) {
   const [pending, setPending] = useState(false);
+  const { auth } = usePage<SharedData>().props;
+
   return (
     <button
-      className="group"
+      className={clsx('group', !auth.user && 'cursor-not-allowed')}
+      disabled={!auth.user}
       onClick={async () => {
         setPending(true);
         const updatedPuppy = await toggleLikedStatus(puppy.id);
@@ -24,7 +29,7 @@ export function LikeToggle({ puppy, setPuppies }: { puppy: Puppy, setPuppies: Di
       ) : (
         <Heart
           className={
-            puppy.likedBy.includes(1)
+            puppy.likedBy.includes(1) && auth.user
               ? "fill-pink-500 stroke-none"
               : "stroke-slate-200 group-hover:stroke-slate-300"
           }
